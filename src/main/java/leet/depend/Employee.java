652aa4da05e28536ac4b39040eef61dd6bcb6f61
e24016611 <sunboyboy1@gmail.com>
2018-01-16 13:08:47 +0800

    dependence

---
+++ src/main/java/leet/depend/Employee.java
@@ -0,0 +1,14 @@
+package leet.depend;
+
+import java.util.List;
+
+// P690
+public class Employee {
+    // It's the unique id of each node;
+    // unique id of this employee
+    public int id;
+    // the importance value of this employee
+    public int importance;
+    // the id of direct subordinates
+    public List<Integer> subordinates;
+};
